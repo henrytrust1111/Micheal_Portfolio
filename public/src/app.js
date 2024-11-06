6e09704de6fd2b5ac0a4c66fd34c5ee106884fd0
henrytrust1111 <henrytrust1111@gmail.com>
2024-11-06 21:30:59 +0100

    second commit9

--- public/src/app.js
+++ public/src/app.js
@@ -1,55 +1,95 @@
-const toggler = document.querySelector('.mode_switch');
-const backToTop = document.querySelector('.back_to_top');
-const htmlEl = document.querySelector('html');
+const toggler = document.querySelector(".mode_switch");
+const backToTop = document.querySelector(".back_to_top");
+const htmlEl = document.querySelector("html");
 
 const toggleMode = () => {
-  if (htmlEl.classList.contains('dark')) {
-    htmlEl.classList.remove('dark');
-    localStorage.setItem('theme', 'light');
+  if (htmlEl.classList.contains("dark")) {
+    htmlEl.classList.remove("dark");
+    localStorage.setItem("theme", "light");
   } else {
-    htmlEl.classList.add('dark');
-    localStorage.setItem('theme', 'dark');
+    htmlEl.classList.add("dark");
+    localStorage.setItem("theme", "dark");
   }
 };
 
 function addThemeToStorage() {
-  const savedTheme = localStorage.getItem('theme');
+  const savedTheme = localStorage.getItem("theme");
   if (savedTheme) {
     htmlEl.classList.add(`${savedTheme}`);
   }
 }
 
-const hamburgerBtn = document.querySelector('.hamBtn');
-const menu = document.querySelector('.menu');
-const header = document.querySelector('header');
+const hamburgerBtn = document.querySelector(".hamBtn");
+const menu = document.querySelector(".menu");
+const header = document.querySelector("header");
+const nav = document.querySelector(".nav");
+const nav2 = document.querySelector(".nav2");
+const nav3 = document.querySelector(".nav3");
+const nav4 = document.querySelector(".nav4");
+const nav5 = document.querySelector(".nav5");
 
-hamburgerBtn.addEventListener('click', (e) => {
-  hamburgerBtn.classList.toggle('open');
-  menu.classList.toggle('open');
-  document.querySelector('body').classList.toggle('hide');
-  console.log('object');
+hamburgerBtn.addEventListener("click", (e) => {
+  hamburgerBtn.classList.toggle("open");
+  menu.classList.toggle("open");
+  // document.querySelector('body').classList.toggle('hide');
+  console.log("object");
+});
+
+nav.addEventListener("click", (e) => {
+  hamburgerBtn.classList.toggle("open");
+  menu.classList.toggle("open");
+  // document.querySelector('body').classList.toggle('hide');
+  console.log("Hello");
+});
+
+nav2.addEventListener("click", (e) => {
+  hamburgerBtn.classList.toggle("open");
+  menu.classList.toggle("open");
+  // document.querySelector('body').classList.toggle('hide');
+  console.log("Hello2");
+});
+
+nav3.addEventListener("click", (e) => {
+  setTimeout(() => {
+    hamburgerBtn.classList.toggle("open");
+    menu.classList.toggle("open");
+  }, 1000);
+});
+
+nav4.addEventListener("click", (e) => {
+  setTimeout(() => {
+    hamburgerBtn.classList.toggle("open");
+    menu.classList.toggle("open");
+  }, 1000);
+});
+
+nav5.addEventListener("click", (e) => {
+  setTimeout(() => {
+    hamburgerBtn.classList.toggle("open");
+    menu.classList.toggle("open");
+  }, 1000);
 });
 
 window.onscroll = () => {
-  header.classList.add('active');
-  backToTop.classList.add('active');
+  header.classList.add("active");
+  backToTop.classList.add("active");
 };
 
-window.addEventListener('scroll', function () {
+window.addEventListener("scroll", function () {
   if (document.documentElement.scrollTop === 0) {
-    header.classList.remove('active');
-    backToTop.classList.remove('active');
+    header.classList.remove("active");
+    backToTop.classList.remove("active");
   }
 });
 
 function initSwiper() {
-  const swiper = new Swiper('.swiper', {
+  const swiper = new Swiper(".swiper", {
     slidesPerView: 1.2,
     spaceBetween: 10,
     freeMode: true,
     autoHeight: true,
     pagination: {
-      el: '.swiper-pagination',
+      el: ".swiper-pagination",
       dynamicBullets: true,
     },
     breakpoints: {
@@ -68,8 +108,8 @@ function initSwiper() {
 
 function init() {
   addThemeToStorage();
-  toggler.addEventListener('click', toggleMode);
+  toggler.addEventListener("click", toggleMode);
   initSwiper();
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
